3f0e5943dad0f54031ee24f0059738fe0a7fe6db
JD <jdw0069@auburn.edu>
2022-11-27 10:13:14 -0600

    convert Student class to JSON Object and output to localhost

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -14,7 +14,7 @@ public class Student {
 
     }
 
-    public Student(Long id, String name, Integer age, LocalDate dob, String email) {
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
         this.id = id;
         this.name = name;
         this.age = age;
