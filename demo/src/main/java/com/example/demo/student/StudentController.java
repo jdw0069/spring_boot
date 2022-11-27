ee9bf421dcc596d6d3efee4d24c69e0eae988718
JD <jdw0069@auburn.edu>
2022-11-27 10:37:23 -0600

    add Service layer to seperate out Controller logic

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,7 +1,5 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "api/v1/students")
 public class StudentController {
+
+	private final StudentService studentService;
+
+
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
     
     @GetMapping
 	public List<Student> getStudents() {
-		return List.of(
-			new Student( 
-				1L,
-				"Mariam",
-				"mariam.test@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
+		return studentService.getStudents();
 	}
 }
